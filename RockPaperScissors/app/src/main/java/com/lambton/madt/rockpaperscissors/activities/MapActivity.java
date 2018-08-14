a04a1598eb0bb788d28708acdb3b1a9e7f8ed54e
Franco Marques <francopilloto@gmail.com>
2018-08-14 12:49:59 -0400

    Including map and location traccking

--- RockPaperScissors/app/src/main/java/com/lambton/madt/rockpaperscissors/activities/MapActivity.java
+++ RockPaperScissors/app/src/main/java/com/lambton/madt/rockpaperscissors/activities/MapActivity.java
@@ -1,35 +1,148 @@
 package com.lambton.madt.rockpaperscissors.activities;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 import com.lambton.madt.rockpaperscissors.R;
+import com.lambton.madt.rockpaperscissors.proximity.TrackingService;
 import com.lambton.madt.rockpaperscissors.utils.IConstants;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import timber.log.Timber;
 
-public class MapActivity extends BaseActivity {
+public class MapActivity extends BaseActivity implements OnMapReadyCallback,
+    TrackingService.TrackingListener
+{
+    
+    private GoogleMap map;
+    private TrackingService trackingService;
+    private Location location;
+    private Marker playerMarker;
+    
+    private static final int PERMISSIONS_REQUEST = 100;
+    
+/* --------------------------------------------------------------------------------------------- */
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        ButterKnife.bind(this);
+        
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        
+        trackingService = new TrackingService(this, this);
+        
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            finish();
+        }
+        
+        int permission = ContextCompat.checkSelfPermission(this,
+        	Manifest.permission.ACCESS_FINE_LOCATION);
+        
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            trackingService.start();
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this,
+                                              new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                              PERMISSIONS_REQUEST);
+        }
+    }
+    
+/* --------------------------------------------------------------------------------------------- */
+    
+    @Override
+    protected void onDestroy()
+    {
+        trackingService.stop();
+        super.onDestroy();
+    }
+    
+/* --------------------------------------------------------------------------------------------- */
+    
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        map = googleMap;
+        
+        if (location != null) {
+            locationChanged(location);
+        }
+    }
+    
+/* --------------------------------------------------------------------------------------------- */
+    
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        if (requestCode == PERMISSIONS_REQUEST && grantResults.length == 1
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            trackingService.start();
+        }
+        else
+        {
+            Toast.makeText(this, "Please enable location services to allow GPS tracking",
+                           Toast.LENGTH_SHORT).show();
+        }
+    }
+    
+/* --------------------------------------------------------------------------------------------- */
+    
+    @Override
+    public void locationChanged(Location location)
+    {
+        this.location = location;
+        
+        if (map == null) {
+            return;
+        }
+        
+        LatLng latLong = new LatLng(location.getLatitude(), location.getLongitude());
+        
+        if (playerMarker != null) {
+            playerMarker.setPosition(latLong);
+        }
+        else {
+            playerMarker = map.addMarker(new MarkerOptions().title("PlayerName").position(latLong));
+        }
+        
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLong, map.getMaxZoomLevel()));
+    }
+    
+/* --------------------------------------------------------------------------------------------- */
+
 	public static void startMapActivity(Context context) {
 		Intent intent = new Intent(context, MapActivity.class);
 		context.startActivity(intent);
-	}
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map);
-		ButterKnife.bind(this);
-
-
 	}
 
 	@Override
---
+++ RockPaperScissors/app/src/main/java/com/lambton/madt/rockpaperscissors/proximity/TrackingService.java
@@ -0,0 +1,93 @@
+package com.lambton.madt.rockpaperscissors.proximity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class TrackingService
+{
+    private Activity activity;
+    private TrackingListener listener;
+
+    private FusedLocationProviderClient client;
+    private LocationCallback locationCallback;
+    private LocationRequest request;
+
+/* --------------------------------------------------------------------------------------------- */
+
+    public interface TrackingListener {
+        void locationChanged(Location location);
+    }
+
+/* --------------------------------------------------------------------------------------------- */
+
+    public TrackingService(Activity activity, TrackingListener listener)
+    {
+        this.activity = activity;
+        this.listener = listener;
+
+        locationCallback = new LocationCallback()
+        {
+            @Override
+            public void onLocationResult(LocationResult locationResult)
+            {
+                Location location = locationResult.getLastLocation();
+
+                if (location != null) {
+                    updateLocation(location);
+                }
+            }
+        };
+
+        client = LocationServices.getFusedLocationProviderClient(activity);
+
+        request = new LocationRequest();
+        request.setInterval(2000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+/* --------------------------------------------------------------------------------------------- */
+
+    public void start()
+    {
+        Log.d("LOCATION", "start");
+
+        int permission = ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            client.requestLocationUpdates(request, locationCallback, null);
+        }
+    }
+
+/* --------------------------------------------------------------------------------------------- */
+
+    public void stop()
+    {
+        Log.d("LOCATION", "stop");
+        client.removeLocationUpdates(locationCallback);
+    }
+
+/* --------------------------------------------------------------------------------------------- */
+
+    private void updateLocation(Location location)
+    {
+        // TODO update firebase here
+        Log.d("LOCATION", location.getLatitude() + ":" + location.getLongitude());
+
+        if (listener != null) {
+            listener.locationChanged(location);
+        }
+    }
+
+/* --------------------------------------------------------------------------------------------- */
+
+}
